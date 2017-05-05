410ff6f3951e62e40c62418f7c3bbb0b886e9862
AntonioZen <a.gromadskiy2015@gmail.com>
2017-05-05 21:22:48 +0300

    Update DataPreparationVariables.py

--- DataPreparationVariables.py
+++ DataPreparationVariables.py
@@ -11,6 +11,7 @@ MONTH = [1,2,3,4,5,6]
 df = data_train
 for M in MONTH:
     data = data_base2.loc[data_base2['MONTH_AGO'] == M]
+    data = data.drop(['MONTH_AGO'], axis=1)
     df = df.merge(data,on='ID',how='left', suffixes=('', '_MONTH_'+str(M)))
     print(df)
     print('_MONTH_'+str(M))
@@ -19,3 +20,27 @@ print(Fore.CYAN)
 print(df.head())
 
 df.to_csv('C:/Users/antonio/Desktop/Kievs/train_base2.csv', sep = ',')
+
+datasetX_mean = df.mean()
+datasetX_std = df.std()
+
+X_variables = df.columns.tolist()
+print(X_variables)
+print(type(X_variables))
+X_variables[0:2] = []
+
+print(X_variables)
+for col in X_variables:
+    print(col)
+    #cok = unicode(col, "utf-8")
+    df[col] = df[col].clip(
+        datasetX_mean[col]-2.6*datasetX_std[col],
+        datasetX_mean[col]+2.6*datasetX_std[col])
+
+print(df)
+
+datasetX_mean2 = df.mean()
+
+df.fillna(datasetX_mean2, inplace=True)
+print(Fore.LIGHTYELLOW_EX)
+print(df)
